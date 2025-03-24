911208bddf3a55ff2552f611784958c2a7f84fc9
Levshin-ArtQ <114755543+Levshin-ArtQ@users.noreply.github.com>
2025-03-24 11:58:44 +0500

    token work

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ window.onload = () => {
   document.getElementById("button").onclick = () => {
     window.YaAuthSuggest.init(
       {
-        client_id: "c46f0c53093440c39f12eff95a9f2f93",
+        client_id: "51ea177b9654430f84b347c87bcbddcc",
         response_type: "token",
-        redirect_uri: "https://examplesite.com/suggest/token",
+        redirect_uri: "https://oauth-padavan.vercel.app/token.html",
       },
-      "https://examplesite.com",
+      "https://oauth-padavan.vercel.app/",
       {
         view: "button",
         parentId: "buttonContainer",
