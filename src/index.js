3ef6168f698292b2e87d18881f6b2de8ae4fe923
Levshin-ArtQ <114755543+Levshin-ArtQ@users.noreply.github.com>
2025-03-24 13:22:55 +0500

    avatar fetch attempt

--- src/index.js
+++ src/index.js
@@ -18,7 +18,30 @@ window.onload = () => {
       }
     )
       .then(({ handler }) => handler())
-      .then((data) => console.log("Сообщение с токеном", data))
+      .then((data) => {
+        localStorage.setItem("accessToken", data.access_token);
+        authorize(fetchYandexData());
+        console.log("Сообщение с токеном", data)
+      })
       .catch((error) => console.log("Обработка ошибки", error));
   };
 };
+
+const authorize = ({
+  default_avatar_id: defaultAvatarId,
+  default_name: displayName,
+}) => {
+  const avatarHtml = `<div className="avatar" style="background-image:url('https://avatars.mds.yandex.net/get-yapic/${defaultAvatarId}/islands-middle')"></div>`;
+  const nameHtml = `<div className="name">${displayName}</div>`;
+  document.getElementById("auth").innerHTML = avatarHtml + nameHtml;
+}
+
+const fetchYandexData = async () => {
+  const accessToken = localStorage.getItem("accessToken");
+  const response = await fetch(`https://login.yandex.ru/info?forma=json`, {
+    headers: {
+      Authorization: `OAuth ${accessToken}`,
+    },
+  });
+  return await response.json();
+}
